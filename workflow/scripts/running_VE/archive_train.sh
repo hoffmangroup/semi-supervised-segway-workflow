2850f6742c8ac727d63a2c0764a5c83da49a3d50
mmendez12 <mendez.mickael@gmail.com>
2022-10-12 00:23:09 -0400

    fix segway scripts and add HepG2

---
+++ workflow/scripts/running_VE/archive_train.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -o nounset -o pipefail -o errexit
+
+TARGET="/params/params.params"
+TRAIN_DIR=${1/"${TARGET}"/}
+
+ARCHIVES="accumulators "
+# ARCHIVES+="auxiliary "
+ARCHIVES+="cmdline "
+ARCHIVES+="intermediate "
+ARCHIVES+="likelihood "
+ARCHIVES+="log "
+ARCHIVES+="observations "
+ARCHIVES+="output "
+ARCHIVES+="triangulation"
+
+for archive in ${ARCHIVES}; do
+# 	echo ${archive}
+	tar -cvz -C ${TRAIN_DIR}/${archive} . -f ${TRAIN_DIR}/${archive}.tar.gz
+done
